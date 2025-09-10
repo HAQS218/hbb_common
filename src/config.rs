cb163e91f3dd3b13b98bf4766bc7b58b00bde370
黑暗骑士 <125466818+HAQS218@users.noreply.github.com>
2025-09-10 14:41:03 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.ljh218.com"];
+pub const RS_PUB_KEY: &str = "ybtKJWOGgLCdnXC27cCTsi8xurW8H2qENoDK7tBg7xU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
